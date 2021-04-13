82c430bd5dcb7bda7070514d98e21a9564b28ae3
Michael Peters <michaelpeterswa@icloud.com>
2021-04-13 13:48:50 -0700

    Update createpaper.sh

--- createpaper.sh
+++ createpaper.sh
@@ -40,7 +40,7 @@ download_and_install () {
 		echo "Jarfile verified."
 		echo "eula=true" > eula.txt
 		echo "Installing start script..."
-		curl -sSL $START_SCRIPT -o "start.sh"
+		curl -sSL $START_SCRIPT > start.sh
 		echo "Starting Server."
 		java -Xms4G -Xmx4G -jar $JAR
 	else
